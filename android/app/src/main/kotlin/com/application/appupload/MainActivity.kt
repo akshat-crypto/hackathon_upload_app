33112674b80d815740e0b4ce6a27c8446922865a
akshat-crypto <akshatsoni396@gmail.com>
2022-03-24 22:37:38 +0530

    first_push

---
+++ android/app/src/main/kotlin/com/application/appupload/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.application.appupload
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/upload_image.dart
@@ -0,0 +1,127 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+
+class UploadImageToFirebase extends StatefulWidget {
+  const UploadImageToFirebase({Key? key}) : super(key: key);
+
+  @override
+  State<UploadImageToFirebase> createState() => _UploadImageToFirebaseState();
+}
+
+class _UploadImageToFirebaseState extends State<UploadImageToFirebase> {
+  File? _imageFile = null;
+  final picker = ImagePicker();
+
+  Future pickImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = File(pickedFile!.path);
+    });
+  }
+
+  Future uploadImageToFirebase(BuildContext context) async {
+    String fileName = basename(_imageFile!.path);
+    // FirebaseStorage ref =
+    Reference ref =
+        FirebaseStorage.instance.ref().child('uploads').child('/$fileName');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: 360,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(250.0),
+                    bottomRight: Radius.circular(10.0)),
+                gradient: LinearGradient(
+                    colors: [Colors.green, Colors.orange],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "Uploading Image to Firebase Storage",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: double.infinity,
+                        margin: const EdgeInsets.only(
+                            left: 30.0, right: 30.0, top: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: _imageFile != null
+                              ? Image.file(_imageFile!)
+                              : FlatButton(
+                                  child: const Icon(
+                                    Icons.add_a_photo,
+                                    color: Colors.blue,
+                                    size: 50,
+                                  ),
+                                  onPressed: pickImage,
+                                ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                uploadImageButton(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget uploadImageButton(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            margin: const EdgeInsets.only(
+                top: 30, left: 20.0, right: 20.0, bottom: 20.0),
+            decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Colors.orange, Colors.green],
+                ),
+                borderRadius: BorderRadius.circular(30.0)),
+            child: FlatButton(
+              // onPressed: () => uploadImageToFirebase(context),
+              onPressed: () {},
+              child: const Text(
+                "Upload Image",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
